33ea9ccddec9055710080ffef184c96d76f48100
Redstonerayy <U25a5DM501B9SH5r0@protonmail.com>
2021-03-09 22:40:04 +0100

    Chosed the Browserview. Now making the menu for the frameless window. Added images for the menu.

--- README.md
+++ README.md
@@ -3,10 +3,17 @@
 # Build
 
 ```code()
-git clone 
+git clone
 cd browser-try
 npm install
 npm update
 npm start
 npm run make
 ```
+
+
+#Ressources
+
+https://github.com/binaryfunt/electron-seamless-titlebar-tutorial
+https://www.electronjs.org/docs/api/browser-view
+https://www.electronjs.org/docs/api/frameless-window
--- src/main.js
+++ src/main.js
@@ -1,17 +1,44 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserView, BrowserWindow } = require('electron')
 
-function createWindow(){
+function createWindow(width, height, frame, load){
   	const win = new BrowserWindow({
-    	width: 800,
-    	height: 600,
+    	width: width,
+    	height: height,
+		frame: frame,
     	webPreferences: {
-      	nodeIntegration: true
+      		nodeIntegration: true
     	}
  	});
-  	win.loadFile('html/index.html')
+  	win.loadFile(load);
+	return win;
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+	const mainwindow = createWindow(1000, 750, false, 'src/html/index.html');
+	//mainwindow.removeMenu(); frame: false removes this
+	/*
+	const view = new BrowserView();
+	const sview = new BrowserView();
+	mainwindow.setBrowserView(view)
+	mainwindow.setBrowserView(sview)
+	view.setBounds({ x: 0, y: 50, width: 1000, height: 200 })
+	view.webContents.loadURL('https://electronjs.org')
+	//view.setAutoResize({width: true, height: true});
+	sview.setBounds({ x: 0, y: 50, width: 1000, height: 200 })
+	sview.webContents.loadURL('https://github.com')
+
+	a = true;
+	setInterval(() => {
+		if(a){
+			mainwindow.setBrowserView(sview);
+			a = false;
+		} else {
+			mainwindow.setBrowserView(view);
+			a = true;
+		}
+	}, 5000);
+	*/
+});
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
